d577f4b759e9862402e2729281f78bb02a6a00fd
pahariyatri <bhanusharm122@gmail.com>
2022-11-01 10:02:51 +0530

    commit five

--- src/Example.js
+++ src/Example.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function example(props) {
   return (
     <div>
-        <h2 style={{color:'#61DBFB',fontFamily: 'Poor Story', cursive}}>{props.title}</h2>
+        <h2 style={{color:'#61DBFB',fontFamily: 'Poor Story'}}>{props.title}</h2>
     </div>
   )
 }
